65604e69f0c596a15a3c6cb4bdcd59786be2d508
Angie Maidt <angiemaidt@gmail.com>
2022-06-23 15:44:56 -0600

    redux store setup done

--- frontend/shmedium/src/index.js
+++ frontend/shmedium/src/index.js
@@ -1,11 +1,33 @@
+//pkg imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+//file imports
 import './index.css';
 import App from './App';
+import configureStore from './store';
+
+//for to test store functionality on window
+const store = configureStore();
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+//root
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
