a55a1815cf889d8219968269bde0589684c648bc
Angie Maidt <angiemaidt@gmail.com>
2022-06-23 11:14:42 -0600

    setup middleware

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+//pkg imports
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+//checks env
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+//init app
+const app = express();
+
+//middleware
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+  }
+// helmet helps set a variety of headers to better secure your app
+app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
+// Set the _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
