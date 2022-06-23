d9d60fba16bcfd5aeb99e16a158092cb5ce58428
Angie Maidt <angiemaidt@gmail.com>
2022-06-23 11:29:50 -0600

    api routes setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,15 @@
+//pkg imports
 const express = require('express');
+//file imports
+const apiRouter = require('./api');
+
+//router init
 const router = express.Router();
 
+//use /api for all routes
+router.use('/api', apiRouter);
+
+//test route
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
