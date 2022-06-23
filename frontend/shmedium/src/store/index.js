20c8c50d24cb8cc6f271e81fb6886702cc9c366a
Angie Maidt <angiemaidt@gmail.com>
2022-06-23 15:40:15 -0600

    /store/index skeleton done

---
+++ frontend/shmedium/src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+//all other reducers go in here
+const rootReducer = combineReducers({
+});
+
+//setting up env enhancers
+let enhancer;
+if (process.env.NODE_ENV === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require('redux-logger').default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+//for to create the store
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+  };
+
+  export default configureStore;
