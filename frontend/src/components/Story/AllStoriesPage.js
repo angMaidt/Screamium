e514da9d8f6fa2440891686293b8cdc7b1c71e05
Angie Maidt <angiemaidt@gmail.com>
2022-06-26 20:51:10 -0600

    stories rendered on /stories

---
+++ frontend/src/components/Story/AllStoriesPage.js
@@ -0,0 +1,1 @@
+//Todo: Render all stories within this component
--- frontend/src/components/Story/index.js
+++ frontend/src/components/Story/index.js
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getAllStories } from '../../store/story'
+
 function Story() {
+    const dispatch = useDispatch();
+    const stories = useSelector(state => state.story)
+    // console.log(typeof stories); =obj
+    useEffect(() => {
+        dispatch(getAllStories())
+    }, [dispatch])
+
     return (
-        <h2>Welcome to stories!</h2>
+        Object.values(stories).map(story => (
+            <div key={story.id} className='story-container'>
+                <h2 class='story-title'>{story.title}</h2>
+                <p class='story-body'>{story.body}</p>
+            </div>
+        ))
     )
 }
 
