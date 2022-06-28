b377dfdd27660df94941afaeb48f9a283003ec9f
Angie Maidt <angiemaidt@gmail.com>
2022-06-28 16:35:51 -0600

    Stories page showing username and number of comments

--- frontend/src/components/Story/AllStories.js
+++ frontend/src/components/Story/AllStories.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { getAllStories } from '../../store/story'
-import Story from './index'
 
 function AllStories() {
     const dispatch = useDispatch();
@@ -13,16 +12,14 @@ function AllStories() {
         dispatch(getAllStories())
     }, [dispatch])
 
-    // const handleStoryClick = (e) => {
-    //     e.preventDefault()
-    // }
-
     return (
         Object.values(stories).map(story => (
             <Link style={{textDecoration: 'none'}} key={story.id} to={`/stories/${story.id}`}>
                 <div className='story-container' key={story.id}>
                     <h2>{story.title}</h2>
+                    <h3>{story.User.username}</h3>
                     <p>{story.body}</p>
+                    <p>{Object.keys(story.Comments).length} comments</p>
                 </div>
             </Link>
             // <div onClick={(e) => handleStoryClick(e)}>
