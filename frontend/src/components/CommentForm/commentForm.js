f10750bf697f4113b91292c44c56839b52964e22
Angie Maidt <angiemaidt@gmail.com>
2022-06-30 14:13:09 -0600

    comment form up and working

--- frontend/src/components/CommentForm/commentForm.js
+++ frontend/src/components/CommentForm/commentForm.js
@@ -8,12 +8,9 @@ function CommentForm () {
     const { storyId } = useParams();
     const [body, setBody] = useState('');
     const [hasSubmitted, setHasSubmitted] = useState(false);
+    const [showCommentForm, setShowCommentForm] = useState(true)
 
     const user = useSelector(state => state.session.user);
-
-    // useEffect(() => {
-    //     dispatch(getAllComments(storyId))
-    // }, [hasSubmitted])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -25,24 +22,45 @@ function CommentForm () {
             storyId,
             body
         }
-        console.log(comment)
         await dispatch(createNewComment(comment))
     }
 
     return (
         <div className='comment-form-container' style={{border:'solid 1px red'}}>
-            <form className='comment-form'
-            onSubmit={handleSubmit}>
-                <input
-                    type='text'
-                    placeholder='What are your thoughts?'
-                    value={body}
-                    onChange={e => setBody(e.target.value)}
-                ></input>
-                <button type='submit'>Respond</button>
-                <button>Cancel</button>
-            </form>
-
+            {showCommentForm &&
+            <div className='comment-form-wrapper'>
+                <div>
+                    <h4 htmlFor='user-name'>{user.username}</h4>
+                </div>
+                <form className='comment-form'
+                onSubmit={handleSubmit}>
+                    <input
+                        type='text'
+                        placeholder='What are your thoughts?'
+                        value={body}
+                        onChange={e => setBody(e.target.value)}
+                    ></input>
+                    <button
+                        type='submit'
+                        disabled={!body}
+                    >Respond</button>
+                    <button
+                        onClick={() => setShowCommentForm(false)}
+                    >Cancel</button>
+                </form>
+            </div>
+            }
+            {!showCommentForm &&
+                <div className='dummy-comment-form-container'>
+                    <form className='dummy-comment-form'>
+                        <input
+                        type='text'
+                        placeholder='What are your thoughts?'
+                        onClick={() => setShowCommentForm(true)}
+                        ></input>
+                    </form>
+                </div>
+            }
         </div>
     )
 }
--- frontend/src/components/Story/AllStories.js
+++ frontend/src/components/Story/AllStories.js
@@ -10,12 +10,15 @@ import { resetAllComments } from '../../store/comment';
 function AllStories() {
     const dispatch = useDispatch();
     const stories = useSelector(state => state.story)
+    const comments = useSelector(state => state.comments)
 
     const [imageSrc, setImgSrc] = useState();
 
     useEffect(() => {
         const fetchData = async() => {
-            dispatch(getAllStories())
+            await dispatch(getAllStories())
+            //TODO: Need to refactor this page so it displays story components, pass comments down as props
+            // await dispatch(getAllComments(storyId))
             // dispatch(resetAllComments())
         }
         fetchData().catch(console.error)
