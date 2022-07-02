9912eae958d5cb8ab51948682a426c4d3ed43669
Angie Maidt <angiemaidt@gmail.com>
2022-07-02 14:38:56 -0600

    login form styled

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
+import './LoginForm.css';
 
 function LoginForm() {
   const dispatch = useDispatch();
@@ -37,29 +38,44 @@ function LoginForm() {
           <li key={idx}>{error}</li>
         ))}
       </ul>
-      <label>
-        Username or Email
-        <input
-          type="text"
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button
-        type="submit"
-        style={{ height:'20px' }}
-      >Log In</button>
-      <button onClick={handleDemoLogin}>Demo-Login</button>
+      <div className='login-form-container'>
+        <div className='welcome-container'>
+          <h2 id='welcome'>Welcome Back.</h2>
+        </div>
+        <div className='input-wrapper'>
+          <div className="username-container">
+            <label id='username-label'>
+              Username or Email:
+            </label>
+            <input
+              type="text"
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+          </div>
+          <div className='password-container'>
+            <label id='password-label'>
+              Password:
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+        <button
+        id='login-submit'
+          type="submit"
+          style={{ height:'20px' }}
+        >Log In</button>
+        <div className='linebreak'></div>
+        <div className='demo-button-container'>
+          <button id='demo-login' onClick={handleDemoLogin}>Demo-Login</button>
+        </div>
+      </div>
     </form>
   );
 }
