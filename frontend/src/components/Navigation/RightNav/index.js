2fec32134e73c7d953d379cf6104eaf86972d4ca
Angie Maidt <angiemaidt@gmail.com>
2022-07-02 20:09:46 -0600

    genre cards looking better

--- frontend/src/components/Navigation/RightNav/index.js
+++ frontend/src/components/Navigation/RightNav/index.js
@@ -1,58 +1,57 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import './RightNav.css';
 
 function RightNav() {
 
     return (
-        <div
-            style={{
-                backgroundColor: 'rgb(5, 4, 5)',
-                height: '100vh',
-                width: '25vw',
-            }}
-            className='right-nav-wrapper'>
-                <div
-                    style={{
-                        position: 'fixed',
-                        right: '0',
-                        backgroundColor: 'rgb(5, 4, 5)',
-                        height: '100vh',
-                        width: '25vw',
-                    }}
-                    className="right-nav-container">
+        // <div className='right-nav-wrapper'>
+                <div className="right-nav-container">
                     <div className='genre-card-wrapper'>
-                        <div style={{ color: 'white'}}>
-                            <Link to='/classic-horror'>
-                                <h3>Classic Horror</h3>
-                            </Link>
-                        </div>
-                        <div style={{ color: 'white'}}>
-                            <Link to='/dark-fantasy'>
+                        <NavLink style={{ textDecoration: 'none', color: 'white' }} to='/classic-horror'>
+                            <div
+                            className='genre-card'
+                            style={{ backgroundColor: '#0347AD' }}>
+                                    <h3>Classic Horror</h3>
+                            </div>
+                        </NavLink>
+                        <NavLink style={{ textDecoration: 'none', color: 'white' }} to='/dark-fantasy'>
+                            <div
+                            className='genre-card'
+                            style={{ backgroundColor: '#032A85' }}>
                                 <h3>Dark Fantasy</h3>
-                            </Link>
-                        </div>
-                        <div style={{ color: 'white'}}>
-                            <Link to='/psychological'>
-                                <h3>Psychological</h3>
-                            </Link>
-                        </div>
-                        <div style={{ color: 'white'}}>
-                            <Link to='/sci-fi'>
-                                <h3>Sci-Fi Horror</h3>
-                            </Link>
-                        </div>
-                        <div style={{ color: 'white'}}>
-                            <Link to='/supernatural'>
+                            </div>
+                        </NavLink>
+                        <NavLink style={{ textDecoration: 'none', color: 'white' }} to='/psychological'>
+                        <div
+                            className='genre-card'
+                            style={{ backgroundColor: '#071E7A' }}>
+                                    <h3>Psychological</h3>
+                            </div>
+                        </NavLink>
+                        <NavLink style={{ textDecoration: 'none', color: 'white' }} to='/sci-fi'>
+                        <div
+                            className='genre-card'
+                            style={{ backgroundColor: '#030E6E' }}>
+                                    <h3>Sci-Fi Horror</h3>
+                            </div>
+                        </NavLink>
+                        <NavLink style={{ textDecoration: 'none', color: 'white' }} to='/supernatural'>
+                        <div
+                            className='genre-card'
+                            style={{ backgroundColor: '#000033' }}>
                                 <h3>Supernatural</h3>
-                            </Link>
-                        </div>
-                        <div style={{ color: 'white'}}>
-                            <Link to='/weird-tales'>
-                                <h3>Weird Tales</h3>
-                            </Link>
-                        </div>
+                            </div>
+                        </NavLink>
+                        <NavLink style={{ textDecoration: 'none', color: 'white' }} to='/weird-tales'>
+                        <div
+                            className='genre-card'
+                            style={{ backgroundColor: 'rgb( 5, 4, 5)' }}>
+                                    <h3>Weird Tales</h3>
+                            </div>
+                        </NavLink>
                     </div>
                 </div>
-            </div>
+            // </div>
     )
 }
 
