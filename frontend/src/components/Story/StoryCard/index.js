439bcbc41518d47dc73df6ff946fd4a2e4db3686
Angie Maidt <angiemaidt@gmail.com>
2022-07-02 22:51:14 -0600

    almost there on story cards

--- frontend/src/components/Story/StoryCard/index.js
+++ frontend/src/components/Story/StoryCard/index.js
@@ -1,60 +1,71 @@
 import { Link } from 'react-router-dom';
 import './StoryCard.css'
 
-function StoryCard({ story }) {
+export const genreNameParser = (genreId) => {
+    let genreName, genreColor
 
-    const genreNameParser = (genreId) => {
-        let genreName
-
-        if (genreId === 1) {
-            genreName = 'Classic Horror'
-        } else if (genreId === 2) {
-            genreName = 'Weird Tales'
-        } else if (genreId === 3) {
-            genreName = 'Dark Fantasy'
-        } else if (genreId === 4) {
-            genreName = 'Sci-Fi Horror'
-        } else if (genreId === 5) {
-            genreName = 'Psychological'
-        } else if (genreId === 6) {
-            genreName = 'Supernatural'
-        }
-        return genreName;
+    if (genreId === 1) {
+        genreColor= '#0347AD'
+        genreName = 'Classic Horror'
+    } else if (genreId === 2) {
+        genreColor= 'rgb( 5, 4, 5)'
+        genreName = 'Weird Tales'
+    } else if (genreId === 3) {
+        genreColor = '#032A85'
+        genreName = 'Dark Fantasy'
+    } else if (genreId === 4) {
+        genreColor = '#030E6E'
+        genreName = 'Sci-Fi Horror'
+    } else if (genreId === 5) {
+        genreColor = '#071E7A'
+        genreName = 'Psychological'
+    } else if (genreId === 6) {
+        genreColor = '#000033'
+        genreName = 'Supernatural'
     }
+    return [ genreName, genreColor ];
+}
 
+function StoryCard({ story }) {
     const genre = genreNameParser(story?.genreId)
+    const [ genreName, genreColor ] = genre
 
     return (
         <div className="story-card-container">
-                <Link style={{textDecoration: 'none'}} key={story.id} to={`/stories/${story.id}`}>
-                    <div className='story-container'
-                    key={story.id}
-                    style={{borderBottom: '1px solid black'}}>
-                        {genre &&
-                            <div
-                                className='story-image-container'>
-                                    {genre}
-                            </div>
-                        }
-                        <div className='story-all-text-container'>
+            <Link style={{textDecoration: 'none', color: 'black'}} key={story.id} to={`/stories/${story.id}`}>
+                <div className='story-container'
+                key={story.id}
+                style={{borderBottom: '1px solid black'}}>
+                    {genre &&
+                        <div
+                            className='story-image-container'
+                            style={{ backgroundColor: genreColor }}>
+                                {/* {genre} */}
+                        </div>
+                    }
+                    <div className='story-all-text-container'>
+                        {/* <div className='card-top'> */}
                             <h2 className='story-title'>{story.title}</h2>
-                            {story.User && <h3 className='story-username'>by {story.User.username}</h3>}
-                            <div className='story-body-container'>
-                                <p className='story-body'>{story.body}</p>
+                            {story.User && <p className='story-username'>by {story.User.username}</p>}
+                        {/* </div> */}
+                        <div className='story-body-container'>
+                            <p className='story-body'>{story.body}</p>
+                        </div>
+                        <div className='story-footer-elements-container'>
+                            <p className='story-comments'>{story.Comments && story.Comments.length} comments</p>
+                            <div className='time-to-read-container'>
+                                {story.body && story.body.length <= 100 && <p className='time-to-read'> 1 min read</p>}
+                                {story.body && story.body.length >= 101 && story.body.length <= 999 && <p className='time-to-read'> 3 min read</p>}
+                                {story.body && story.body.length >= 1000 && story.body.length <= 2000 && <p className='time-to-read'> 5 min read</p>}
+                                {story.body && story.body.length >= 2001 && <p className='time-to-read'> 10+ min read</p>}
                             </div>
-                            <div className='story-footer-elements-container'>
-                                <p className='story-comments'>{story.Comments && story.Comments.length} comments</p>
-                                <div className='time-to-read-container'>
-                                    {story.body && story.body.length <= 100 && <p className='time-to-read'> 1 min read</p>}
-                                    {story.body && story.body.length >= 101 && story.body.length <= 999 && <p className='time-to-read'> 3 min read</p>}
-                                    {story.body && story.body.length >= 1000 && story.body.length <= 2000 && <p className='time-to-read'> 5 min read</p>}
-                                    {story.body && story.body.length >= 2001 && <p className='time-to-read'> 10+ min read</p>}
-                                </div>
-                                <div className='genre-container'>{genre}</div>
+                            <div className='genre-container' style={{ backgroundColor: genreColor }} >
+                                <p>{genreName}</p>
                             </div>
                         </div>
                     </div>
-                </Link>
+                </div>
+            </Link>
         </div>
     )
 }
